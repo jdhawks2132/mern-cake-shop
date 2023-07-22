955e5c1bf9e8e1ad63b81b5358eb6da7997fff23
Joshua Hawks <jdhawks@gmail.com>
2023-07-22 06:48:21 -0500

    break point adjustment

--- client/src/components/cards/CardContainer.jsx
+++ client/src/components/cards/CardContainer.jsx
@@ -8,7 +8,7 @@ const CardContainer = ({cakes, page}) => {
   })
 
   return (
-    <div className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4 m-11'>
+    <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 m-11'>
       {cakeCards}
     </div>
   )
