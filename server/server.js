48e458d6639d67c19f4974a609df1cbcefac7c90
Joshua Hawks <jdhawks@gmail.com>
2023-07-21 23:01:48 -0500

    server.js was only serving home route

--- server/server.js
+++ server/server.js
@@ -20,11 +20,13 @@ app.use(express.json());
 
 if (process.env.NODE_ENV === 'production') {
     app.use(express.static(path.join(__dirname, '../client/dist')));
-  }
   
-  app.get('/', (req, res) => {
+  
+  app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, '../client/dist/index.html'));
   });
+
+}
   
   // Create a new instance of an Apollo server with the GraphQL schema
   const startApolloServer = async () => {
